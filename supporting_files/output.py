9d6bf7f314a8a35bb4f87bc06d0d35838cf8c153
Pratik Kinage <37847256+thirstycode@users.noreply.github.com>
2018-03-30 05:09:26 +0530

    todo

--- supporting_files/output.py
+++ supporting_files/output.py
@@ -1,3 +1,3 @@
 def output():
     # motor rotate function
-    print("motor Output Function Goes Here")
+    print("motor Output Function Goes Here .. .. Turning Right . If You Are On LAptop Please Rotate Lap in Right")
