6058f14eacf4a1f6a25805085fe584d7b51cea74
Pratik Kinage <37847256+thirstycode@users.noreply.github.com>
2018-03-30 05:08:00 +0530

    todo

--- supporting_files/stitch.py
+++ supporting_files/stitch.py
@@ -8,6 +8,7 @@ import cv2
 def stitch(initial,changing):
 	# # load the two images and resize them to have a width of 400 pixels
 	# # (for faster processing)
+	# #	TODO
 	stitcher = Stitcher()
 	result= stitcher.stitch([initial, changing])
 	return result
